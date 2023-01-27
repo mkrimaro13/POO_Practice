5c7070ababddb9be9d02c4efeaf5fbda60115aa0
Marco Osorio <marco.osorio86@hotmail.com>
2023-01-27 14:23:17 -0500

    Creating two new Java Clases

--- POO/Java/carSubclases/CarVan.java
+++ POO/Java/carSubclases/CarVan.java
@@ -6,11 +6,11 @@ import Java.Car;
 import java.util.ArrayList;
 import java.util.Map;
 
-public class CarBlack extends Car {
+public class CarVan extends Car {
     Map<String,Map<String,String>> typeCarAccepted; // Se utiliza el Map porque se necesita: <Marca,<Modelo, Año>>
     ArrayList<String> seatsMaterial;
 
-    public CarBlack(String license, Account driver, Integer passenger, Map<String,Map<String,String>> typeCarAccepted, ArrayList<String> seatsMaterial) {
+    public CarVan(String license, Account driver, Integer passenger, Map<String,Map<String,String>> typeCarAccepted, ArrayList<String> seatsMaterial) {
         super(license, driver, passenger);
         this.typeCarAccepted = typeCarAccepted;
         this.seatsMaterial = seatsMaterial;
